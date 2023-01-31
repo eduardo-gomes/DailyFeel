7788447dedbd6568a092156a11b19fd0e034e526
Eduardo M Gomes <eduardo.2001.06@gmail.com>
2023-01-31 13:33:39 -0300

    Template for typescript web app with solid
    Took 29 minutes

---
+++ build.js
@@ -0,0 +1,17 @@
+import { build } from "esbuild";
+import { solidPlugin } from "esbuild-plugin-solid";
+import fs from "fs";
+
+build({
+	entryPoints: ["./src/index.ts"],
+	bundle: true,
+	sourcemap: true,
+	minify: false,
+	format: "esm",
+	outfile: "out/bundled.js",
+	logLevel: "info",
+	plugins: [solidPlugin()]
+}).catch(() => process.exit(1));
+
+fs.mkdirSync("./out", {recursive: true});
+fs.copyFileSync("./src/index.html", "./out/index.html");
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import "./style.css";
+import { render } from "solid-js/web";
+
+render(() => "Hello world!", document.getElementById("App") as HTMLDivElement);
