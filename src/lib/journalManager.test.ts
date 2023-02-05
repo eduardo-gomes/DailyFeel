1f69df0b1b473c29654fa7d52ca2ebbffbf0dfc7
Eduardo M Gomes <eduardo.2001.06@gmail.com>
2023-02-05 17:32:53 -0300

    Create presistency across journalManager instances using singleton, also clear manager after every test

--- src/lib/journalManager.test.ts
+++ src/lib/journalManager.test.ts
@@ -1,41 +1,45 @@
 import { JournalManager } from "./journalManager";
 import { Entry } from "./journalTypes";
 
+function clearManager() {
+	new JournalManager().clear();
+}
+
+afterEach(clearManager);
+beforeAll(clearManager);
+
 test("New journal manager has no entries", () => {
 	const manager = new JournalManager();
 	let entryList = manager.get_entry_list();
 	expect(entryList.length).toBe(0);
-})
+});
 
 test("New journal after add entries, has one entry", () => {
 	const manager = new JournalManager();
 	let content = {
-		mood: 0,
-		text: "ASD"
+		mood: 0, text: "ASD"
 	};
 	manager.new_entry(new Date(), content);
 	let entryList = manager.get_entry_list();
 	expect(entryList.length).toBe(1);
-})
+});
 
 test("New journal after add entries, get same content and date", () => {
 	const manager = new JournalManager();
 	let content = {
-		mood: 0,
-		text: "ASD"
+		mood: 0, text: "ASD"
 	};
 	let date = new Date();
 	manager.new_entry(date, content);
 	let got_entry = manager.get_entry_list().at(0) as Entry;
 	expect(got_entry.content).toEqual(content);
 	expect(got_entry.date).toEqual(date);
-})
+});
 
 test("Changes on the added entry are not visible", () => {
 	const manager = new JournalManager();
 	let content = {
-		mood: 0,
-		text: "ASD"
+		mood: 0, text: "ASD"
 	};
 	let date = new Date();
 	manager.new_entry(date, content);
@@ -51,13 +55,12 @@ test("Changes on the added entry are not visible", () => {
 		expect(got_entry.content).not.toEqual(content);
 		expect(got_entry.date).not.toEqual(date);
 	}
-})
+});
 
 test("Got entries are not writable", () => {
 	const manager = new JournalManager();
 	let content = {
-		mood: 0,
-		text: "ASD"
+		mood: 0, text: "ASD"
 	};
 	let date = new Date();
 	manager.new_entry(date, content);
@@ -68,4 +71,28 @@ test("Got entries are not writable", () => {
 	expect(() => {
 		got_entry.date = new Date();
 	}).toThrow();
-})
+});
+
+test(".clear removes all entries", () => {
+	const manager = new JournalManager();
+	let content = {
+		mood: 0, text: "ASD"
+	};
+	manager.new_entry(new Date(), content);
+	manager.clear();
+	let entries = manager.get_entry_list();
+	expect(entries).toHaveLength(0);
+});
+
+test("Manager persists entries", () => {
+	{
+		const manager = new JournalManager();
+		let content = {
+			mood: 0, text: "ASD"
+		};
+		manager.new_entry(new Date(), content);
+	}
+	const manager = new JournalManager();
+	let entries = manager.get_entry_list();
+	expect(entries).toHaveLength(1);
+});
--- src/lib/journalManager.ts
+++ src/lib/journalManager.ts
@@ -9,7 +9,14 @@ function deepFreeze<T extends Object>(obj: T) {
 }
 
 class JournalManager {
+	static instance: JournalManager;
 	array: Array<Entry> = [];
+
+	constructor() {
+		if (JournalManager.instance)
+			return JournalManager.instance;
+		JournalManager.instance = this;
+	}
 
 	new_entry(date: Date, entry: EntryContent) {
 		let items = structuredClone({date, content: entry});
@@ -18,6 +25,10 @@ class JournalManager {
 
 	get_entry_list(): readonly Entry[] {
 		return this.array;
+	}
+
+	clear() {
+		this.array.length = 0;
 	}
 }
 
