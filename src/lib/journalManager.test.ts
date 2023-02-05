0ffb1719b12c91729203a564c92f254f997625b3
Eduardo M Gomes <eduardo.2001.06@gmail.com>
2023-02-05 17:57:51 -0300

    Remove redundants SAMPLE_CONTENT by using global constant

--- src/lib/journalManager.test.ts
+++ src/lib/journalManager.test.ts
@@ -1,6 +1,10 @@
 import { JournalManager } from "./journalManager";
 import { Entry } from "./journalTypes";
 
+const SAMPLE_CONTENT = {
+	mood: 0, text: "ASD"
+};
+
 function clearManager() {
 	new JournalManager().clear();
 }
@@ -16,54 +20,42 @@ test("New journal manager has no entries", () => {
 
 test("New journal after add entries, has one entry", () => {
 	const manager = new JournalManager();
-	let content = {
-		mood: 0, text: "ASD"
-	};
-	manager.new_entry(new Date(), content);
+	manager.new_entry(new Date(), SAMPLE_CONTENT);
 	let entryList = manager.get_entry_list();
 	expect(entryList.length).toBe(1);
 });
 
 test("New journal after add entries, get same content and date", () => {
 	const manager = new JournalManager();
-	let content = {
-		mood: 0, text: "ASD"
-	};
 	let date = new Date();
-	manager.new_entry(date, content);
+	manager.new_entry(date, SAMPLE_CONTENT);
 	let got_entry = manager.get_entry_list().at(0) as Entry;
-	expect(got_entry.content).toEqual(content);
+	expect(got_entry.content).toEqual(SAMPLE_CONTENT);
 	expect(got_entry.date).toEqual(date);
 });
 
 test("Changes on the added entry are not visible", () => {
 	const manager = new JournalManager();
-	let content = {
-		mood: 0, text: "ASD"
-	};
 	let date = new Date();
-	manager.new_entry(date, content);
+	manager.new_entry(date, SAMPLE_CONTENT);
 	{
 		let got_entry = manager.get_entry_list().at(0) as Entry;
-		expect(got_entry.content).toEqual(content);
+		expect(got_entry.content).toEqual(SAMPLE_CONTENT);
 		expect(got_entry.date).toEqual(date);
 	}
-	content.text = "Other";
+	SAMPLE_CONTENT.text = "Other";
 	date.setDate(0);
 	{
 		let got_entry = manager.get_entry_list().at(0) as Entry;
-		expect(got_entry.content).not.toEqual(content);
+		expect(got_entry.content).not.toEqual(SAMPLE_CONTENT);
 		expect(got_entry.date).not.toEqual(date);
 	}
 });
 
 test("Got entries are not writable", () => {
 	const manager = new JournalManager();
-	let content = {
-		mood: 0, text: "ASD"
-	};
 	let date = new Date();
-	manager.new_entry(date, content);
+	manager.new_entry(date, SAMPLE_CONTENT);
 	let got_entry = manager.get_entry_list().at(0) as Entry;
 	expect(() => {
 		got_entry.content.text = "Other";
@@ -75,10 +67,7 @@ test("Got entries are not writable", () => {
 
 test(".clear removes all entries", () => {
 	const manager = new JournalManager();
-	let content = {
-		mood: 0, text: "ASD"
-	};
-	manager.new_entry(new Date(), content);
+	manager.new_entry(new Date(), SAMPLE_CONTENT);
 	manager.clear();
 	let entries = manager.get_entry_list();
 	expect(entries).toHaveLength(0);
@@ -87,10 +76,7 @@ test(".clear removes all entries", () => {
 test("Manager persists entries", () => {
 	{
 		const manager = new JournalManager();
-		let content = {
-			mood: 0, text: "ASD"
-		};
-		manager.new_entry(new Date(), content);
+		manager.new_entry(new Date(), SAMPLE_CONTENT);
 	}
 	const manager = new JournalManager();
 	let entries = manager.get_entry_list();
