6985a467e7566ca5bf8e6dd5d4b1aa80631e7b4c
Eduardo M Gomes <eduardo.2001.06@gmail.com>
2023-02-06 11:20:27 -0300

    Improve test run time by using temporary directory to store database

--- src/lib/indexeddb_node.ts
+++ src/lib/indexeddb_node.ts
@@ -1,9 +1,17 @@
+import { join } from "path";
+import { mkdirSync } from "fs";
+import { tmpdir } from "os";
+
 const setGlobalVars = require("indexeddbshim");
 // @ts-ignore
 // noinspection JSConstantReassignment
 global.window = {};
 // @ts-ignore
 global.location = {origin: "localhost"};
-setGlobalVars();
+
+const path = join(tmpdir(), "dailyfeel");
+mkdirSync(path, {recursive: true});
+
+setGlobalVars(window, {databaseBasePath: path});
 
 if (typeof window.indexedDB != "object") throw new Error("indexedDB not properly defined");
